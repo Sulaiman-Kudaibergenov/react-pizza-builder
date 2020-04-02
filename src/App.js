1064cb4afa05b64f3cef3a185cbe9445742d8c43
Sulaiman <sulaimankudaibergenov@gmail.com>
2020-04-02 22:15:21 +0600

    Added my message

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        Здесь был Сулайман
         <BrowserRouter>
           <Layout>
             <Route path="/" component={PizzaBuilder} exact />
